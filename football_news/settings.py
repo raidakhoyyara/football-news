febb66cc46ca2feb446588200056be5135350c16
raidakhoyyara <raidakhoyyara247@gmail.com>
2025-08-27 16:29:34 +0700

    update

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://pbp.cs.ui.ac.id/raida.khoyyara/footballnews"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "raida-khoyyara-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
